90024735ca7803c144fe8b43e97d15b248e50975
Alejandro Mazzeo <ale.exactas@gmail.com>
2020-06-11 20:17:14 -0300

    Added setup and __init__ files

---
+++ catmu/__init__.py
@@ -0,0 +1,15 @@
+"""
+    catmu
+    ~~~~~~~~~~~~~
+    Convolution Accelerated by the Texture Mapping Unit of GPU.
+    :copyright: 2020 by catmu Authors, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from . import analysis_tools, tmu_simulator, wrapper
+
+__all__ = [
+    analysis_tools,
+    tmu_simulator,
+    wrapper
+]
