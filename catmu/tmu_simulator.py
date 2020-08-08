a33393129d3d3046501de825cd3a106237032d7b
Alejandro Mazzeo <ale.exactas@gmail.com>
2020-08-08 02:34:25 -0300

    TMU simulator now accept outrange values

--- catmu/tmu_simulator.py
+++ catmu/tmu_simulator.py
@@ -14,48 +14,81 @@ def tex2d(x: float, y: float, psf: np.ndarray):
     T = psf
 
     """
-    x = np.array(x)
-    y = np.array(y)
+    psf = np.pad(psf, pad_width=1, mode='constant', constant_values=0)
+    m, n = psf.shape
 
-    mask_x = np.logical_or(x < 0, x > 1.0)
-    mask_y = np.logical_or(y < 0, y > 1.0)
-    mask = np.logical_or(mask_x, mask_y)
+    x = np.array(x, dtype=float)
+    y = np.array(y, dtype=float)
 
-    x[x < 0] = 0.0
-    x[x > 1.0] = 1.0
-    y[y < 0] = 0.0
-    y[y > 1.0] = 1.0
+    # mask_x = np.logical_or(x < 0, x > 1.0)
+    # mask_y = np.logical_or(y < 0, y > 1.0)
+    # mask = np.logical_or(mask_x, mask_y)
 
-    x = x * (psf.shape[1] - 1) + 0.5
-    y = y * (psf.shape[0] - 1) + 0.5
+    x += 1.0
+    y += 1.0
 
-    a, i = np.modf(x - 0.5)
-    b, j = np.modf(y - 0.5)
+    x[x < 0.5] = 0.5
+    x[x > n] = n
+    y[y < 0.5] = 0.5
+    y[y > m] = m
+
+    a, i = np.modf(y - 0.5)
+    b, j = np.modf(x - 0.5)
 
     i = np.asarray(i, dtype=int)
     j = np.asarray(j, dtype=int)
 
+    ip = np.clip(i+1, 0, m-1)
+    jp = np.clip(j+1, 0, n-1)
+
     t1 = (1 - a) * (1 - b) * psf[i, j]
-    t2 = a * (1 - b) * psf[np.minimum(psf.shape[0] - 1, i + 1), j]
-    t3 = (1 - a) * b * psf[i, np.minimum(psf.shape[1] - 1, j + 1)]
-    t4 = a * b * psf[np.minimum(psf.shape[1] - 1, i + 1), np.minimum(psf.shape[0] - 1, j + 1)]
+    t2 = a * (1 - b) * psf[ip, j]
+    t3 = (1 - a) * b * psf[i, jp]
+    t4 = a * b * psf[ip, jp]
 
     result = t1 + t2 + t3 + t4
-    result[mask] = 0.0
+    # result[mask] = 0.0
     return result
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
-    psf_lut = np.arange(9).reshape((3, 3))
+    # psf_lut = np.arange(9).reshape((3, 3))
+    # n = 100
+    # x, y = 2 * np.mgrid[0:n + 1, 0:n + 1] / n - 0.5
+    # print(x, y)
+    # texture_applied = tex2d(x=x, y=y, psf=psf_lut)
+    # print(texture_applied)
+    # _, (ax1, ax2) = plt.subplots(1, 2)
+    # ax1.imshow(psf_lut, extent=(0, 1, 0, 1), vmin=np.min(psf_lut), vmax=np.max(psf_lut))
+    # ax2.imshow(texture_applied, vmin=np.min(psf_lut), vmax=np.max(psf_lut),
+    #            extent=(np.min(x), np.max(x), np.min(y), np.max(y)))
+    # plt.show()
+
     n = 100
-    x, y = 2 * np.mgrid[0:n + 1, 0:n + 1] / n - 0.5
-    print(x, y)
-    texture_applied = tex2d(x=x, y=y, psf=psf_lut)
-    print(texture_applied)
-    _, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.imshow(psf_lut, extent=(0, 1, 0, 1), vmin=np.min(psf_lut), vmax=np.max(psf_lut))
-    ax2.imshow(texture_applied, vmin=np.min(psf_lut), vmax=np.max(psf_lut),
-               extent=(np.min(x), np.max(x), np.min(y), np.max(y)))
+    pad = 3
+
+    psf = np.pad(np.ones((11, 11)), pad_width=pad, mode='constant', constant_values=0)
+
+    y, x = np.mgrid[0:n * (psf.shape[0]), 0:n * (psf.shape[1])] / n
+
+    plt.imshow(tex2d(x, y, psf), extent=(x.min(), x.max(), y.min(), y.max()))
+
+    for i in range(psf.shape[0]):
+        if i == pad or i == psf.shape[0] - pad:
+            plt.axhline(i, color='red')
+        else:
+            plt.axhline(i)
+    for i in range(psf.shape[1]):
+        if i == pad or i == psf.shape[1] - pad:
+            plt.axvline(i, color='red')
+        else:
+            plt.axvline(i)
+
     plt.show()
+
+
+
+
+
